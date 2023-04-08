64c77a0291826241623b1f379c52c7cc6383c9d5
MT224244 <25514849+MT224244@users.noreply.github.com>
2023-04-09 07:27:49 +0900

    🐛 BASE_URLが正しくない問題を修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const repo = process.env.GITHUB_REPOSITORY;
-const base = `/${repo?.split('/')[1] ?? ''}`;
+const base = repo ? `/${repo.split('/')[1]}/` : '/';
 
 const mode = process.env.NODE_ENV;
 const isDev = mode === 'development';
