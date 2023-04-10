c377a9449ca1c689e04668bdd315559eac625009
MT224244 <25514849+MT224244@users.noreply.github.com>
2023-04-11 03:48:30 +0900

    💄 ヘッダーのスタイルを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useMatches } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import { Avatar, Breadcrumbs, Link, Header as PrimerHeader } from '@primer/react';
+import { Box, Breadcrumbs, Link, Header as PrimerHeader } from '@primer/react';
 import { VersionSelector } from './VersionSelector';
 import { RouterLink } from './RouterLink';
 
@@ -20,34 +20,41 @@ export const Header = () => {
                 <link rel='preload' as='image' type='image/png' href={`${BASE_URL}icon.png`} />
             </Helmet>
 
-            <PrimerHeader>
-                <Avatar
-                    src='icon.png'
-                    alt=''
-                    size={32}
-                    sx={{
-                        marginRight: 3,
-                        boxShadow: 'none',
-                    }}
-                />
-                <Breadcrumbs>
-                    {crumbs.map(crumb => {
-                        const url = new URL(location.href
-                            .replace('/#', '')
-                            .replace(BASE_URL, '/'),
-                        );
-                        return (
-                            url.pathname === crumb.path
-                                ? <span key={crumb.path}>{crumb.name}</span>
-                                : <Link
-                                    key={crumb.path}
-                                    as={RouterLink}
-                                    to={crumb.path}
-                                >{crumb.name}</Link>
-                        );
-                    })}
-                </Breadcrumbs>
-                <VersionSelector sx={{ marginLeft: 4 }} />
+            <PrimerHeader sx={{ display: 'block' }}>
+                <Box
+                    display='flex'
+                    flexWrap='wrap'
+                    alignItems='center'
+                    maxWidth='xlarge'
+                    marginX='auto'
+                >
+                    <Box
+                        as='img'
+                        src='icon.png'
+                        alt=''
+                        width='32px'
+                        height='32px'
+                        marginRight='3'
+                    />
+                    <Breadcrumbs sx={{ whiteSpace: 'nowrap' }}>
+                        {crumbs.map(crumb => {
+                            const url = new URL(location.href
+                                .replace('/#', '')
+                                .replace(BASE_URL, '/'),
+                            );
+                            return (
+                                url.pathname === crumb.path
+                                    ? <span key={crumb.path}>{crumb.name}</span>
+                                    : <Link
+                                        key={crumb.path}
+                                        as={RouterLink}
+                                        to={crumb.path}
+                                    >{crumb.name}</Link>
+                            );
+                        })}
+                    </Breadcrumbs>
+                    <VersionSelector sx={{ marginLeft: 4 }} />
+                </Box>
             </PrimerHeader>
         </>
     );
