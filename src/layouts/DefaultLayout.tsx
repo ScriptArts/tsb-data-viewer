b2295464649bd88ff01ad4a9619100c14c54d1cb
MT224244 <25514849+MT224244@users.noreply.github.com>
2023-04-21 13:58:17 +0900

    🚸 ヘッダーをstickyに変更

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,19 @@
 import { Outlet } from 'react-router-dom';
+import { Box } from '@primer/react';
 import { Header } from '../components/Header';
 
 export const DefaultLayout = () => {
     return (
         <>
+            <Box
+                position='sticky'
+                top='0'
+                left='0'
+                width='100vw'
+                zIndex='999'
+            >
             <Header />
+            </Box>
             <Outlet />
         </>
     );
