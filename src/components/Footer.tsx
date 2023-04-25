2ff55a53de29e906d32ffab82873db36c4d16c33
MT224244 <25514849+MT224244@users.noreply.github.com>
2023-04-26 04:39:22 +0900

    💄 Footerのレイアウトを調整

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,18 +3,24 @@ import { Box, Link } from '@primer/react';
 export const Footer = () => {
     return (
         <Box
-            display='flex'
-            alignItems='center'
             height='2rem'
-            paddingX='2'
             backgroundColor='header.bg'
-            sx={{ columnGap: '2' }}
         >
-            <Box as='small'>&copy; 2023 MT224244</Box>
-            <Link
-                href={`https://github.com/${REPO}`}
-                sx={{ color: 'fg.default' }}
-            >GitHub</Link>
+            <Box
+                display='flex'
+                alignItems='center'
+                maxWidth='xlarge'
+                height='100%'
+                marginX='auto'
+                paddingX='2'
+                sx={{ columnGap: '2' }}
+            >
+                <Box as='small'>&copy; 2023 MT224244</Box>
+                <Link
+                    href={`https://github.com/${REPO}`}
+                    sx={{ color: 'fg.default' }}
+                >GitHub</Link>
+            </Box>
         </Box>
     );
 };
